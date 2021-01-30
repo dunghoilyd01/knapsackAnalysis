0950cbc63f143999cdb8248a222fd9ff324f2552
Huy <johnmactavish123@gmail.com>
2021-01-30 16:02:17 +0700

    Complete the analysis for Recursive implementation

--- analysisR.py
+++ analysisR.py
@@ -4,8 +4,12 @@ import time
 
 for i in range (1,4):
     W, n, wt, val, result = inputReader.input_processing("Test"+str(i)+".txt")
-    a = knapsackR.knapsackRecursion(W, n, wt, val)
-    time_start = time.time()
-    a.Solve()
-    print("Ket qua = ", a.ketqua, "| So phep toan = ", a.complexity)
+    for CustomW in range (W, W + 50, 10):
+        a = knapsackR.knapsackRecursion(CustomW, n, wt, val)
+        start_time = time.time()
+        a.Solve()
+        print("Test case: ",i, "Custom W:", CustomW)
+        print("Giai bai toan trong:",time.time() - start_time)
+        print("Voi so phep toan: ", a.complexity)
+ 
 
